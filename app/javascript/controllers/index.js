bde52b31c14bb22c0f64080bc6fdc34013fafe7b
Ruslan <oganer@gmail.com>
2021-06-29 01:42:02 +0300

    feat: add stimulus-controller-resolver package

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,11 @@
-// Load all the controllers within this directory and all subdirectories. 
+// Load all the controllers within this directory and all subdirectories.
 // Controller files must be named *_controller.js.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from 'stimulus'
+import StimulusControllerResolver from 'stimulus-controller-resolver'
 
 const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+
+StimulusControllerResolver.install(application, async controllerName => (
+    (await import(`../controllers/${controllerName}_controller.js`)).default
+))
